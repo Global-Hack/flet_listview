eadbe8b6a17063b7d972ffce9a6319a58d8b6b9d
Global-Hack <global_hack_com_2017@yahoo.co.jp>
2023-01-09 11:08:23 +0900

    flet_listview test loop

--- flet_listview/main.py
+++ flet_listview/main.py
@@ -1,9 +1,23 @@
+from time import sleep
 import flet as ft
 
 def main(page: ft.Page):
-    for i in range(5000):
-        page.controls.append(ft.Text(f"Line {i}"))
-    page.scroll = "always"
-    page.update()
+    page.title = "Auto-scrolling ListView"
 
-ft.app(target=main, view=ft.WEB_BROWSER)
+    lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+
+    count = 1
+
+    for i in range(0, 60):
+        lv.controls.append(ft.Text(f"Line {count}"))
+        count += 1
+
+    page.add(lv)
+
+    for i in range(0, 60):
+        sleep(1)
+        lv.controls.append(ft.Text(f"Line {count}"))
+        count += 1
+        page.update()
+
+ft.app(target=main)
