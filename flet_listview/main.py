c02531c7755065141827884cfde594b63255cf45
Global-Hack <global_hack_com_2017@yahoo.co.jp>
2023-01-09 11:12:07 +0900

    flet_listview test insert loop

--- flet_listview/main.py
+++ flet_listview/main.py
@@ -4,19 +4,19 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Auto-scrolling ListView"
 
-    lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+    lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=False)
 
     count = 1
 
     for i in range(0, 60):
-        lv.controls.append(ft.Text(f"Line {count}"))
+        lv.controls.insert(0,ft.Text(f"Line {count}"))
         count += 1
 
     page.add(lv)
 
     for i in range(0, 60):
         sleep(1)
-        lv.controls.append(ft.Text(f"Line {count}"))
+        lv.controls.insert(0,ft.Text(f"Line {count}"))
         count += 1
         page.update()
 
